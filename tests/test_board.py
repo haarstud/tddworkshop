d3be4fd29680b72e4bf5af2af735e3aeda487ff0
Yoav Kleinberger <haarcuba@gmail.com>
2024-07-20 16:29:09 +0300

    test for complete game

--- tests/test_board.py
+++ tests/test_board.py
@@ -10,3 +10,12 @@ def test_no_winner_on_incomplete_game():
     b.make_move(what='O', where='b')
     b.make_move(what='X', where='e')
     assert b.winner() is None
+
+def test_winner_on_row():
+    b = board.Board()
+    b.make_move(what='X', where='a')
+    b.make_move(what='O', where='b')
+    b.make_move(what='X', where='e')
+    b.make_move(what='O', where='d')
+    b.make_move(what='X', where='i')
+    assert b.winner() == 'X'
