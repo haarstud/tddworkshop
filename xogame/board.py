b548c4cc74d7623712ff4b807ef45c7148ea592a
Yoav Kleinberger <haarcuba@gmail.com>
2024-07-20 16:35:41 +0300

    pass win test for X and O

--- xogame/board.py
+++ xogame/board.py
@@ -1,10 +1,27 @@
 class Board:
     def __init__(self):
-        self._winner = None
+        self._board = {position: None for position in 'abcdefghi'}
 
     def winner(self):
-        return self._winner
+        for symbol in 'XO':
+            if self._won(symbol):
+                return symbol
+
+        return None
+
+    def _won(self, symbol):
+        for row in 'abc', 'def', 'ghi':
+            if all(self._board[position] == symbol for position in row):
+                return True
+        for column in 'adg', 'beh', 'cfi':
+            if all(self._board[position] == symbol for position in column):
+                return True
+
+        DIAGONAL = 'aei'
+        if all(self._board[position] == symbol for position in DIAGONAL):
+            return True
+
+        return False
 
     def make_move(self, what, where):
-        if (what, where) == ('X', 'i'):
-            self._winner = 'X'
+        self._board[where] = what
