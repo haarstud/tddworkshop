ddf14558a6095208677373d0e652f00f03263f03
Yoav Kleinberger <haarcuba@gmail.com>
2024-07-20 16:26:20 +0300

    failing test for incomplete game

--- tests/test_board.py
+++ tests/test_board.py
@@ -3,3 +3,10 @@ from xogame import board
 def test_no_winner_on_empty_board():
     b = board.Board()
     assert b.winner() is None
+
+def test_no_winner_on_incomplete_game():
+    b = board.Board()
+    b.make_move(what='X', where='a')
+    b.make_move(what='O', where='b')
+    b.make_move(what='X', where='e')
+    assert b.winner() is None
